614b72dd74a259c57022a1791f9834eff11a1239
Igor Cardoso <igorgcardoso7@gmail.com>
2023-12-20 08:40:44 -0300

    Initial commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,46 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![convert_file,])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn convert_file(path: String, resolution: Resolution) -> Result<String, String> {
+    use std::path::Path;
+    use std::process::Command;
+
+    if !Path::new(&path).exists() {
+        return Err("File does not exist".to_string());
+    }
+
+    let mut binding = Command::new("ffmpeg");
+    let cmd = binding.arg("-i").arg(&path);
+
+    let cmd = match resolution {
+        Resolution::Mobile => cmd.arg("-vf").arg("scale=-1:360"),
+        Resolution::Hd => cmd.arg("-vf").arg("scale=-1:720"),
+        Resolution::Same => cmd,
+    };
+
+    let path = Path::new(&path);
+
+    let output_name = path.file_stem().unwrap().to_str().unwrap().to_string() + ".mp4";
+    let output_path = path.parent().unwrap().join(output_name);
+
+    cmd.arg(output_path.to_str().unwrap())
+        .output()
+        .expect("failed to execute process");
+
+    Ok(output_path.to_str().unwrap().to_string())
+}
+
+#[derive(serde::Deserialize)]
+enum Resolution {
+    Mobile,
+    Hd,
+    Same,
+}
---
+++ src/App.tsx
@@ -0,0 +1,97 @@
+import { invoke } from '@tauri-apps/api';
+import { open } from '@tauri-apps/api/dialog';
+import clsx from 'clsx';
+import { useState } from 'react';
+
+function App() {
+  const [selectedFile, setSelectedFile] = useState('');
+  const [resolution, setResolution] = useState('');
+  const [isConverting, setIsConverting] = useState(false);
+  const [done, setDone] = useState(false);
+
+  async function openFile() {
+    setDone(false);
+    const result = await open({
+      multiple: false,
+      filters: [
+        {
+          name: '*',
+          extensions: ['mp4', 'mkv', 'avi', 'webm'],
+        },
+      ],
+    });
+
+    if (result && typeof result === 'string') {
+      setSelectedFile(result);
+    }
+  }
+
+  async function convertFile() {
+    if (selectedFile) {
+      try {
+        setIsConverting(true);
+        await invoke('convert_file', {
+          path: selectedFile,
+          resolution: resolution || 'Same',
+        });
+        setDone(true);
+        setSelectedFile('');
+      } finally {
+        setIsConverting(false);
+      }
+    }
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-900">
+      <div className="flex flex-col items-center justify-center h-full">
+        <button
+          className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600"
+          onClick={openFile}
+        >
+          Abrir arquivo
+        </button>
+        {selectedFile && (
+          <strong
+            className={clsx('mt-4 text-white text-center', {
+              'text-yellow-400': isConverting,
+              'text-green-500': done,
+            })}
+          >
+            {selectedFile}
+          </strong>
+        )}
+
+        <div className="flex flex-col items-center justify-center mt-4">
+          <label
+            htmlFor="resolution"
+            className="font-bold text-white text-lg leading-relaxed"
+          >
+            Resolução
+          </label>
+          <select
+            name="resolution"
+            id="resolution"
+            className="px-2 py-1 border border-gray-500 rounded-md"
+            defaultValue={'Same'}
+            onChange={(e) => setResolution(e.target.value)}
+          >
+            <option value="Same">Manter</option>
+            <option value="Mobile">360p</option>
+            <option value="Hd">720p</option>
+          </select>
+        </div>
+        {!isConverting && (
+          <button
+            className="px-4 py-2 mt-4 text-white bg-green-500 rounded-md hover:bg-green-600"
+            onClick={convertFile}
+          >
+            Converter
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
